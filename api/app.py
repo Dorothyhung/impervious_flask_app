30d439b58dc7055e70e36b2c77d3ef016dfd2215
Dorothyhung <dorothyh0602@gmail.com>
2023-08-15 21:15:05 -0700

    testing folium map

--- api/app.py
+++ api/app.py
@@ -20,13 +20,13 @@ with rio.open(path) as src:
 # clat = (34.7236241463 + 34.7833234084)/2
 # clon = (-84.5767920522 + -84.519140314)/2
 
-# m = folium.Map(location=[clat, clon], tiles='Stamen Terrain', zoom_start = 13)
-# folium.raster_layers.ImageOverlay(
-#     image=img[0],
-#     name='test',
-#     opacity=1,
-#     bounds=[[34.7236241463, -84.5767920522], [34.7833234084, -84.519140314]],
-# ).add_to(m)
+m = folium.Map(location=[clat, clon], tiles='Stamen Terrain', zoom_start = 13)
+folium.raster_layers.ImageOverlay(
+    image=img[0],
+    name='test',
+    opacity=1,
+    bounds=[[34.7236241463, -84.5767920522], [34.7833234084, -84.519140314]],
+).add_to(m)
 
 # folium.LayerControl().add_to(m)
 # m.save('templates/index.html')
