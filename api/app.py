e43d2f2f53a99cbde45073dab26f16b7e0fdd47a
Dorothyhung <dorothyh0602@gmail.com>
2023-08-15 20:24:50 -0700

    test folium

--- api/app.py
+++ api/app.py
@@ -12,23 +12,23 @@ if __name__ == '__main__':
 
 
 import folium
-import rasterio as rio
+# import rasterio as rio
 
-path = "impervious_rendered.tif"
-with rio.open(path) as src:
-    boundary = src.bounds
-    img = src.read()
+# path = "impervious_rendered.tif"
+# with rio.open(path) as src:
+#     boundary = src.bounds
+#     img = src.read()
 
-clat = (34.7236241463 + 34.7833234084)/2
-clon = (-84.5767920522 + -84.519140314)/2
+# clat = (34.7236241463 + 34.7833234084)/2
+# clon = (-84.5767920522 + -84.519140314)/2
 
-m = folium.Map(location=[clat, clon], tiles='Stamen Terrain', zoom_start = 13)
-folium.raster_layers.ImageOverlay(
-    image=img[0],
-    name='test',
-    opacity=1,
-    bounds=[[34.7236241463, -84.5767920522], [34.7833234084, -84.519140314]],
-).add_to(m)
+# m = folium.Map(location=[clat, clon], tiles='Stamen Terrain', zoom_start = 13)
+# folium.raster_layers.ImageOverlay(
+#     image=img[0],
+#     name='test',
+#     opacity=1,
+#     bounds=[[34.7236241463, -84.5767920522], [34.7833234084, -84.519140314]],
+# ).add_to(m)
 
 # folium.LayerControl().add_to(m)
 # m.save('templates/index.html')
