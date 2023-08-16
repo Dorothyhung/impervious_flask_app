75b2d0f733ac3740c81935de95e2119538bddc38
Dorothyhung <dorothyh0602@gmail.com>
2023-08-15 20:20:02 -0700

    test my index.html

--- api/app.py
+++ api/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+# @app.route('/')
+# def home():
+#     return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
